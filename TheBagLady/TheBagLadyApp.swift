c0be11051e285d1ff4be6d9333bfee3bd2bee4fe
brooke912 <94486596+brooke912@users.noreply.github.com>
2025-05-22 19:40:27 -0500

    Initial Commit

---
+++ TheBagLady/TheBagLadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBagLadyApp.swift
+//  TheBagLady
+//
+//  Created by Brooke Gates on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TheBagLadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheBagLadyTests/TheBagLadyTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheBagLadyTests.swift
+//  TheBagLadyTests
+//
+//  Created by Brooke Gates on 5/22/25.
+//
+
+import Testing
+
+struct TheBagLadyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
